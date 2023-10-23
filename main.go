8b332c87841658f138f9dbbeb4751431c47b2c22
Chuma Umenze <git@chumaumenze.com>
2023-10-23 12:26:45 +0100

    Refactor CLI flags

--- main.go
+++ main.go
@@ -18,82 +18,62 @@ import (
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	ingestCmd := flag.NewFlagSet("ingest", flag.ExitOnError)
-	ingestConfig := ingestCmd.String("config", "config.toml", "")
-	insertCmd := flag.NewFlagSet("insert", flag.ExitOnError)
-	insertConfig := insertCmd.String("config", "config.toml", "")
+	var (
+		ingestMode, insertMode bool
+		configFile             string
+	)
 
-	var configFile string
-
-	if len(os.Args) < 2 {
-		fmt.Println("expected ingest or insert subcommands")
-		os.Exit(1)
-	}
-
-	// Flag for server or consumer mode
-	switch os.Args[1] {
-	case "ingest":
-		ingestCmd.Parse(os.Args[2:])
-		configFile = *ingestConfig
-	case "insert":
-		insertCmd.Parse(os.Args[2:])
-		configFile = *insertConfig
-	default:
-		log.Println("Expected ingest or insert")
-		os.Exit(1)
-	}
+	flag.BoolVar(&ingestMode, "ingest", false, "Run ingestion")
+	flag.BoolVar(&insertMode, "insert", false, "Run imports")
+	flag.StringVar(&configFile, "config", "config.toml", "Path to configuration file")
+	flag.Parse()
 
 	viper.SetConfigFile(configFile)
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(fmt.Errorf("fatal error config file: %w", err))
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal(fmt.Errorf("fatal error config file: %w", err))
 	}
 
-	var C config.Config
-	err = viper.Unmarshal(&C)
-	if err != nil {
+	var cfg config.Config
+	if err := viper.Unmarshal(&cfg); err != nil {
 		log.Fatalf("unable to decode into struct, %v", err)
 	}
 
-	var wg sync.WaitGroup
+	var (
+		wg sync.WaitGroup
+		c  = make(chan os.Signal, 1)
+	)
+	signal.Notify(c, os.Interrupt)
 
-	switch os.Args[1] {
-	case "ingest":
-		i := ingest.NewFileIngest(&C)
-
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
-
-		wg.Add(1)
-		go func() {
-			_ = <-c
-			fmt.Println("Gracefully shutting down import...")
-			_ = i.Stop()
-			wg.Done()
-		}()
-
-		i.Start()
-	case "insert":
-		i := importer.NewImporter(&C)
-
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
+	if ingestMode && insertMode {
+		log.Fatal("ingest and insert are mutually exclusive")
+	} else if ingestMode {
+		i := ingest.NewFileIngest(&cfg)
 
 		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			_ = <-c
-			fmt.Println("Gracefully shutting down insert...")
-			_ = i.Stop()
-			wg.Done()
+			log.Println("gracefully shutting down ingest")
+			if err := i.Stop(); err != nil {
+				log.Println(err)
+			}
 		}()
-
 		i.Start()
-	default:
-		log.Println("Expected ingest or insert")
-		os.Exit(1)
+	} else if insertMode {
+		i := importer.NewImporter(&cfg)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_ = <-c
+			log.Println("gracefully shutting down import")
+			if err := i.Stop(); err != nil {
+				log.Println(err)
+			}
+		}()
+		i.Start()
+	} else {
+		log.Fatal("expected ingest or insert")
 	}
 
 	wg.Wait()
-
 }
