146acd4343ac23559e60fc3140a7bb7bae89ac35
Shane <sd@1li.dev>
2023-12-18 13:37:42 +0000

    build: change BackendStorage's interface to more closely match S3's

--- pkg/filestore/storage.go
+++ pkg/filestore/storage.go
@@ -11,11 +11,11 @@ var (
 
 // StorageBackend is the interface implemented by storage backends
 type StorageBackend interface {
-	// Upload stores the content of reader at path
-	Upload(path string, reader io.Reader) error
+	// Upload stores the content of r at path
+	Upload(path string, r io.ReadSeeker) error
 
-	// Download fetches the content stored at path or returns an error
+	// Download fetches the content stored at path and writes it to w or returns an error
 	//
 	// If path is not found (wrapped) error ErrNotFound is returned
-	Download(path string) (io.ReadCloser, error)
+	Download(path string, w io.WriterAt) error
 }
