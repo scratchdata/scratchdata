af22e18de3d49575f2cdd3e2f04028a43fe927ba
jgoel <jay@hotswap.app>
2024-01-08 09:40:03 -0500

    Revert deletion of routes file

---
+++ cmd/api/routes.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/gofiber/contrib/fiberzerolog"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func (a *API) InitializeAPIServer() error {
+	app := fiber.New()
+	a.app = app
+
+	app.Use(fiberzerolog.New(fiberzerolog.Config{
+		Logger: &log.Logger,
+		Levels: []zerolog.Level{zerolog.ErrorLevel, zerolog.WarnLevel, zerolog.TraceLevel},
+	}))
+
+	// Initialize default config
+	app.Use(cors.New())
+
+	a.app.Get("/healthcheck", a.AuthMiddleware, a.HealthCheck)
+	a.app.Get("/query", a.AuthMiddleware, a.Query)
+	a.app.Post("/query", a.AuthMiddleware, a.Query)
+	a.app.Get("/tables", a.AuthMiddleware, a.Tables)
+	a.app.Post("/data", a.AuthMiddleware, a.Insert)
+
+	err := app.Listen(fmt.Sprintf(":%d", a.config.Port))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
