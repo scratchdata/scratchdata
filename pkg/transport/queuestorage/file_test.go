5f1b5b17d345374674207497a77776806cdeab15
Shane <sd@1li.dev>
2024-01-24 16:04:44 +0000

    increase sleep to 10s

--- pkg/transport/queuestorage/file_test.go
+++ pkg/transport/queuestorage/file_test.go
@@ -186,8 +186,8 @@ func TestFileWriterAutoRotation(t *testing.T) {
 		param := param
 		param.MaxFileAge = 1 * time.Second
 		checkRotation(t, param, func() {
-			// +1s to ensure rotation completes from timer expiry
-			time.Sleep(param.MaxFileAge + (1 * time.Second))
+			// +10s to ensure rotation completes from timer expiry
+			time.Sleep(param.MaxFileAge + (10 * time.Second))
 		})
 	})
 
