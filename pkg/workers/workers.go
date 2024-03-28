248ffb609e6c3d53bb2e2f59255abca265e8d0f5
poundifdef <jay@scratchdata.com>
2024-03-28 16:11:34 -0400

    worker that deadlocks

--- pkg/workers/workers.go
+++ pkg/workers/workers.go
@@ -24,42 +24,118 @@ type ScratchDataWorker struct {
 	destinationManager *destinations.DestinationManager
 }
 
-func (w *ScratchDataWorker) Start(ctx context.Context, threadId int) {
-	log.Debug().Int("thread", threadId).Msg("Starting worker")
-
-	hostname, _ := os.Hostname()
-	workerLabel := fmt.Sprintf("%s-%d", hostname, threadId)
-
+func (w *ScratchDataWorker) Produce(ctx context.Context, ch chan<- *models.Message, wg *sync.WaitGroup, messageType models.MessageType) {
+	defer wg.Done()
 	for {
-		item, ok := w.StorageServices.Database.Dequeue(models.InsertData, workerLabel)
-
-		if !ok {
-			time.Sleep(1 * time.Second)
-		} else {
-			message, err := w.messageToStruct([]byte(item.Message))
-			if err != nil {
-				log.Error().Err(err).Int("thread", threadId).Str("message", item.Message).Msg("Unable to decode message")
-			}
-
-			err = w.processMessage(threadId, message)
-			if err == nil {
-				deleteErr := w.StorageServices.Database.Delete(item.ID)
-				if deleteErr != nil {
-					log.Error().Err(deleteErr).Uint("message_id", item.ID).Msg("Unable to delete message from queue")
-				}
-			} else {
-				log.Error().Err(err).Int("thread", threadId).Interface("message", message).Msg("Unable to process message")
-			}
-		}
-
 		select {
 		case <-ctx.Done():
-			log.Debug().Int("thread", threadId).Msg("Stopping worker")
+			// close(ch)
+			// close producer channel
 			return
 		default:
+			item, ok := w.StorageServices.Database.Dequeue(messageType, "worker-label")
+			if !ok {
+				time.Sleep(1 * time.Second)
+			}
+			ch <- item
 		}
 	}
 }
+
+func (w *ScratchDataWorker) Consume(ctx context.Context, ch <-chan *models.Message, threadId int, wg *sync.WaitGroup) {
+	log.Debug().Int("thread", threadId).Msg("Starting worker")
+	defer wg.Done()
+
+	// hostname, _ := os.Hostname()
+	// workerLabel := fmt.Sprintf("%s-%d", hostname, threadId)
+
+	// queuesToPoll := []models.MessageType{models.InsertData, models.CopyData}
+	// var queueIdxToPoll uint8
+
+	for item := range ch {
+		// queueToPoll := queuesToPoll[queueIdxToPoll]
+		// item, ok := w.StorageServices.Database.Dequeue(queueToPoll, workerLabel)
+
+		message, err := w.messageToStruct([]byte(item.Message))
+		if err != nil {
+			log.Error().Err(err).Int("thread", threadId).Str("message", item.Message).Msg("Unable to decode message")
+			continue
+		}
+
+		switch item.MessageType {
+		case models.InsertData:
+			err = w.processMessage(threadId, message)
+		default:
+			log.Error().Err(err).Int("thread", threadId).Interface("message", item).Msg("Unrecognized message type")
+			continue
+		}
+
+		if err == nil {
+			deleteErr := w.StorageServices.Database.Delete(item.ID)
+			if deleteErr != nil {
+				log.Error().Err(deleteErr).Uint("message_id", item.ID).Msg("Unable to delete message from queue")
+			}
+		} else {
+			log.Error().Err(err).Int("thread", threadId).Interface("message", message).Msg("Unable to process message")
+		}
+
+		// select {
+		// case <-ctx.Done():
+		// log.Debug().Int("thread", threadId).Msg("Stopping worker")
+		// return
+		// default:
+		// }
+	}
+}
+
+// func (w *ScratchDataWorker) Start(ctx context.Context, threadId int) {
+// 	log.Debug().Int("thread", threadId).Msg("Starting worker")
+
+// 	hostname, _ := os.Hostname()
+// 	workerLabel := fmt.Sprintf("%s-%d", hostname, threadId)
+
+// 	queuesToPoll := []models.MessageType{models.InsertData, models.CopyData}
+// 	var queueIdxToPoll uint8
+
+// 	for {
+// 		queueToPoll := queuesToPoll[queueIdxToPoll]
+// 		item, ok := w.StorageServices.Database.Dequeue(queueToPoll, workerLabel)
+
+// 		if !ok {
+// 			time.Sleep(1 * time.Second)
+// 		} else {
+// 			message, err := w.messageToStruct([]byte(item.Message))
+// 			if err != nil {
+// 				log.Error().Err(err).Int("thread", threadId).Str("message", item.Message).Msg("Unable to decode message")
+// 				continue
+// 			}
+
+// 			switch item.MessageType {
+// 			case models.InsertData:
+// 				err = w.processMessage(threadId, message)
+// 			default:
+// 				log.Error().Err(err).Int("thread", threadId).Any("message", item).Msg("Unrecognized msg type")
+// 				continue
+// 			}
+
+// 			if err == nil {
+// 				deleteErr := w.StorageServices.Database.Delete(item.ID)
+// 				if deleteErr != nil {
+// 					log.Error().Err(deleteErr).Uint("message_id", item.ID).Msg("Unable to delete message from queue")
+// 				}
+// 			} else {
+// 				log.Error().Err(err).Int("thread", threadId).Interface("message", message).Msg("Unable to process message")
+// 			}
+// 		}
+
+// 		select {
+// 		case <-ctx.Done():
+// 			log.Debug().Int("thread", threadId).Msg("Stopping worker")
+// 			return
+// 		default:
+// 		}
+// 	}
+// }
 
 func (w *ScratchDataWorker) processMessage(threadId int, message queue_models.FileUploadMessage) error {
 	destination, err := w.destinationManager.Destination(context.TODO(), message.DatabaseID)
@@ -148,18 +224,27 @@ func RunWorkers(ctx context.Context, config config.Workers, storageServices *sto
 		destinationManager: destinationManager,
 	}
 
-	log.Debug().Msg("Starting Workers")
-	var wg sync.WaitGroup
-	i := 0
-	for i = 0; i < config.Count; i++ {
-		wg.Add(1)
-		go func(threadId int) {
-			defer wg.Done()
-			workers.Start(ctx, threadId)
-			log.Print("worker done")
-		}(i)
-	}
-	wg.Wait()
+	var values chan *models.Message
 
-	// Clean up resources and gracefully shut down the web server
+	log.Debug().Msg("Starting Producers")
+	var producerWg sync.WaitGroup
+
+	producerWg.Add(2)
+	go workers.Produce(ctx, values, &producerWg, models.InsertData)
+	go workers.Produce(ctx, values, &producerWg, models.CopyData)
+
+	log.Debug().Msg("Starting Consumers")
+	var consumerWg sync.WaitGroup
+	for i := 0; i < config.Count; i++ {
+		consumerWg.Add(1)
+		go workers.Consume(ctx, values, i, &consumerWg)
+	}
+
+	producerWg.Wait()
+
+	log.Debug().Msg("Closing Producer")
+	close(values)
+
+	log.Debug().Msg("Closing Consumers...")
+	consumerWg.Wait()
 }
