93b32ceb57bd01993027c139363317704f5af80e
Jay <jgoel@jays-air.mynetworksettings.com>
2024-04-24 09:33:35 -0400

    pg

---
+++ pkg/destinations/postgres/insert.go
@@ -0,0 +1,139 @@
+package postgres
+
+import (
+	"fmt"
+
+	"github.com/scratchdata/scratchdata/pkg/util"
+
+	"os"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+)
+
+func (s *PostgresServer) createColumns(table string, jsonTypes map[string]string) error {
+
+	for colName, jsonType := range jsonTypes {
+		colType := "VARCHAR"
+		switch jsonType {
+		case "int":
+			colType = "BIGINT"
+		case "bool":
+			colType = "BOOLEAN"
+		case "float":
+			colType = "DOUBLE PRECISION"
+		case "string":
+			colType = "VARCHAR"
+		default:
+			colType = "VARCHAR"
+		}
+
+		sql := fmt.Sprintf("ALTER TABLE %s ADD COLUMN \"%s\" %s", s.Schema+"."+table, colName, colType)
+		_, err := s.conn.Exec(sql)
+		if err != nil {
+			if !strings.Contains(err.Error(), "already exists") {
+				log.Error().Err(err).Str("column", colName).Msg("createColumns: cannot create column")
+				return err
+			}
+
+		}
+		log.Trace().Str("name", colName).Str("type", colType).Msg("Created column")
+
+	}
+
+	return nil
+}
+func (s *PostgresServer) CreateColumns(table string, fileName string) error {
+
+	input, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	// Infer JSON types for the input
+	jsonTypes, err := util.GetJSONTypes(input)
+	if err != nil {
+		return err
+	}
+
+	err = s.createColumns(table, jsonTypes)
+	if err != nil {
+		return err
+	}
+
+	err = input.Close()
+	if err != nil {
+		log.Error().Err(err).Str("filename", fileName).Msg("Unable to close file")
+	}
+
+	return nil
+
+}
+
+func (s *PostgresServer) CreateEmptyTable(table string) error {
+
+	sql := fmt.Sprintf("CREATE TABLE IF NOT EXISTS \"%s\" (__row_id BIGINT)", table)
+	_, err := s.conn.Exec(sql)
+	return err
+}
+
+func (s *PostgresServer) InsertFromNDJsonFile(table string, filePath string) error {
+	return nil
+	// Make sure the table exists
+
+	// Recalling createColumns to create columns in the table if missing,  will be created
+	// err := s.CreateColumns(table, filePath)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// params := make(map[string]any)
+
+	// params["region"] = s.S3Region
+	// params["access_key_id"] = s.S3AccessKeyId
+	// params["secret_access_key"] = s.S3SecretAccessKey
+	// params["bucket"] = s.S3Bucket
+
+	// s3Client, err := s3.NewStorage(params)
+	// if err != nil {
+	// 	log.Fatal().Err(err).Msg("Unable to create blobstore")
+	// }
+
+	// file, err := os.Open(filePath)
+	// if err != nil {
+	// 	return err
+	// }
+	// defer file.Close()
+
+	// s3FilePath := ""
+	// if s.S3FilePrefix != "" {
+	// 	s3FilePath = s.S3FilePrefix + "/"
+	// }
+	// s3FilePath += table + "/" + filepath.Base(filePath)
+
+	// err = s3Client.Upload(s3FilePath, file)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// if err != nil {
+	// 	return err
+	// }
+
+	// copyCommand := fmt.Sprintf("COPY %s FROM 's3://%s/%s' CREDENTIALS 'aws_access_key_id=%s;aws_secret_access_key=%s' FORMAT AS JSON 'auto'", s.Schema+"."+table, s.S3Bucket, s3FilePath, s.S3AccessKeyId, s.S3SecretAccessKey)
+
+	// _, err = s.conn.Exec(copyCommand)
+	// if err != nil {
+	// 	return err
+	// }
+	// if s.DeleteFromS3 {
+	// 	log.Info().Str("Deleting file %s from S3", s3FilePath)
+	// 	err = s3Client.Delete(s3FilePath)
+
+	// 	if err != nil {
+	// 		log.Error().Err(err).Str("file_path", s3FilePath).Msg("Failed to delete file from S3")
+	// 	}
+
+	// 	log.Info().Str("Deleted file %s from S3", s3FilePath)
+	// }
+	// return nil
+}
---
+++ pkg/destinations/postgres/postgres.go
@@ -0,0 +1,67 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+	"github.com/scratchdata/scratchdata/pkg/util"
+
+	_ "github.com/lib/pq"
+)
+
+type PostgresServer struct {
+	Host     string `mapstructure:"host" schema:"host" form:"label:Host,type:text"`
+	Port     int    `mapstructure:"port" schema:"port" form:"label:Port,type:number,default:5439"`
+	Database string `mapstructure:"database" schema:"database" form:"label:Database,type:text"`
+	Username string `mapstructure:"user" schema:"user" form:"label:User,type:text"`
+	Password string `mapstructure:"password" schema:"password" form:"label:Password,type:password"`
+	Schema   string `mapstructure:"schema" schema:"schema" form:"label:Schema,type:text,default:public"`
+
+	conn *sql.DB
+}
+
+func openConn(s *PostgresServer) (*sql.DB, error) {
+	url := fmt.Sprintf("user=%v password=%v host=%v port=%v dbname=%v connect_timeout=5 sslmode=require",
+		s.Username,
+		s.Password,
+		s.Host,
+		s.Port,
+		s.Database,
+	)
+
+	var err error
+	var db *sql.DB
+
+	if db, err = sql.Open("postgres", url); err != nil {
+		log.Error().Err(err).Msg("postgres conn error")
+		return nil, err
+	}
+	log.Printf("Connecting to Postgres %v", url)
+	if err = db.Ping(); err != nil {
+		log.Error().Err(err).Msg("postgres ping error")
+		return nil, err
+	}
+	log.Info().Msg("Connected to Postgres")
+	return db, nil
+}
+
+func OpenServer(settings map[string]any) (*PostgresServer, error) {
+
+	srv := util.ConfigToStruct[PostgresServer](settings)
+	if srv.Schema == "" {
+		srv.Schema = "public"
+	}
+
+	conn, err := openConn(srv)
+	if err != nil {
+		log.Error().Err(err).Msg("Postgres OpenServer Error")
+		return nil, err
+	}
+	srv.conn = conn
+	return srv, nil
+}
+
+func (s *PostgresServer) Close() error {
+	return s.conn.Close()
+}
