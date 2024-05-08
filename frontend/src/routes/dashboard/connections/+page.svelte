5a4cfcfb14b6eeb01f2309276d7f00d04ec7756e
Cody A. Taylor <codemister99@yahoo.com>
2024-05-08 16:47:59 -0400

    feature: Delete database connection (Part 1) (#171)
    * feature: Delete database connection
    
    * Use connection name in delete dialog

--- frontend/src/routes/dashboard/connections/+page.svelte
+++ frontend/src/routes/dashboard/connections/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     /** @type {import('./$types').PageData} */
 
+    import { onMount } from "svelte";
     import { fade } from "svelte/transition";
 
     export let data;
@@ -16,7 +17,80 @@
     }
 
     let destinations = getDestinations();
+
+    async function sendDeleteConnection(connectionId) {
+        const resource = `/api/destinations/${connectionId}`;
+        const options = {
+            method: 'DELETE',
+        };
+        const response = await fetch(resource, options);
+
+        if (response.ok) {
+            return {status: "success"};
+        } else {
+            const errorMessage = await response.text();
+
+            return {status: "error", errorMessage};
+        }
+    }
+
+    let context = {};
+
+    function showDeleteDialog() {
+        const dest = this.value;
+        const buttonConfirm = context.dialog.querySelector('.dialog-confirm');
+        
+        context.dialog.querySelector('.connection-name').innerText = dest.Name;
+        context.alertSuccess.querySelector('.connection-name').innerText = dest.Name;
+        context.alertError.querySelector('.connection-name').innerText = dest.Name;
+
+        context.connectionId = dest.ID;
+        context.dialog.show();
+    }
+
+    function showErrorAlert(errorText) {
+        const {alertError} = context;
+        alertError.querySelector('.alert-error-text').innerText = errorText;
+        alertError.show();
+    }
+
+    function confirmDelete() {
+        sendDeleteConnection(context.connectionId)
+            .then(({status, errorMessage}) => {
+                if (status === "error") {
+                    showErrorAlert(errorMessage);
+                } else {
+                    destinations = getDestinations();
+                    context.alertSuccess.show();
+                }
+            })
+            .catch(showErrorAlert)
+            .finally(() => context.dialog.hide());
+    }
+
+    onMount(() => {
+        context.dialog = document.querySelector('.dialog-connection-delete');
+        context.alertSuccess = document.querySelector('.alert-success.alert-connection-delete');
+        context.alertError = document.querySelector('.alert-error.alert-connection-delete');
+    });
 </script>
+
+<sl-dialog label="Delete Connection" class="dialog-connection-delete">
+    Are you sure you want to delete <span class="connection-name"></span> connection?
+    <sl-button slot="footer" variant="danger" size="medium" pill class="dialog-confirm" on:click="{confirmDelete}">Confirm</sl-button>
+    <sl-button slot="footer" variant="default" size="medium" pill class="dialog-cancel" on:click="{context.dialog.hide()}">Cancel</sl-button>
+</sl-dialog>
+
+<sl-alert class="alert-success alert-connection-delete" duration="7000">
+    <sl-icon slot="icon" name="info-circle"></sl-icon>
+    Successfully removed <span class="connection-name"></span> connection.
+</sl-alert>
+
+<sl-alert class="alert-error alert-connection-delete" closable>
+    <sl-icon slot="icon" name="exclamation-circle"></sl-icon>
+    Unable to delete <span class="connection-name"></span> connection.
+    <div class="alert-error-text"></div>
+</sl-alert>
 
 <h2>Connections</h2>
 
@@ -37,7 +111,10 @@
         <!-- <p in:fade={{ delay: 101, duration: 100 }}>done {x}</p> -->
         <!-- <p transition:fade> -->
             {#each destList as dest}
-            {dest.ID} - {dest.Type} - {dest.Name} <button>delete</button>
+            {dest.ID} - {dest.Type} - {dest.Name} <sl-button variant="danger" size="small" outline pill value="{dest}" class="button-connection-delete" on:click={showDeleteDialog}>
+                <sl-icon slot="prefix" name="trash"></sl-icon>
+                Delete
+            </sl-button>
             <br>
             {/each}
             <!-- done  -->
