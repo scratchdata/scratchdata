28a6d2a24d9f309e89a9c9793349aa4638469422
Cody Taylor <codemister99@yahoo.com>
2024-05-09 12:24:10 -0400

    Create components for connection parameters

---
+++ frontend/src/lib/DestinationBoolParam.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let name;
+    export let label;
+    export let defaultValue;
+
+    let props = {
+        name,
+    };
+
+    if (typeof defaultValue === "boolean") {
+        props.defaultChecked = defaultValue;
+        props.checked = defaultValue;
+    } else if (defaultValue === "true") {
+        props.defaultChecked = true;
+        props.checked = true;
+    }
+</script>
+
+<sl-switch {...props}>{label}</sl-switch>
---
+++ frontend/src/lib/DestinationTextareaParam.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let name;
+    export let label;
+    export let defaultValue;
+
+    let props = {
+        name,
+        placeholder: label,
+    };
+
+    if (typeof defaultValue === "string" && defaultValue.length > 0) {
+        props.defaultValue = defaultValue;
+        props.value = defaultValue;
+    }
+</script>
+
+<sl-textarea {...props}>
+    <label slot="label">{label}</label>
+</sl-textarea>
